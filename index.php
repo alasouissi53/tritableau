ce0062c3dd6aa6cabaf70afabb9948dc2096548c
alasouissi53 <alasouissi53@gmail.com>
2022-06-29 11:45:51 +0200

    fix

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Insertion</title>
+    <script>
+        function addtolist()
+        {
+            document.getElementById('l').value = document.getElementById('l').value 
+            + ' ' + document.getElementById('c').value 
+        }
+    
+    </script>
+</head>
+
+<body>
+    
+    <form action="calcul.php" method="post">
+        Inserer element: <input type="text" id="c" >
+        <button type="button" onclick="addtolist()"> ADD </button> <br><br><br>
+        Liste des entiers inseres: <input type="text" id="l" name="l">
+        <br><br>
+        <input type="submit">
+    </form>
+</body>
+</html
